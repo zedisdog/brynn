5827d8f00978358b9fc48b6885396cfe057b77c0
zed <zedsmail@163.com>
2023-09-19 19:24:01 +0800

    feat: 基础map解析

--- util/mapx/mapx.go
+++ util/mapx/mapx.go
@@ -1,7 +1,11 @@
 package mapx
 
 import (
+	"errors"
 	"reflect"
+	"strings"
+
+	"github.com/zedisdog/brynn/util/slicex"
 )
 
 func Merge[K comparable, V any](m1 map[K]V, m2 map[K]V) (result map[K]V) {
@@ -24,16 +28,66 @@ func Merge[K comparable, V any](m1 map[K]V, m2 map[K]V) (result map[K]V) {
 	return
 }
 
-// func Unmarshal(m map[string][]any, v any) (err error) {
-// 	value := reflect.ValueOf(v)
-// }
+const (
+	keyName = "input"
+)
 
-func Set(value reflect.Value, v any) (err error) {
-	vv := reflect.ValueOf(v)
-	// fmt.Printf("%+v\n", value.Type().Name())
-	if value.Kind() == reflect.Invalid || value.Kind() == vv.Kind() {
-		value.Set(vv)
+var (
+	ErrNotMatch = errors.New("type not match")
+)
+
+func Unmarshal(src map[string]any, dest any) (err error) {
+	destValue := reflect.ValueOf(dest)
+	if destValue.Kind() != reflect.Pointer || !destValue.Elem().IsValid() {
+		return errors.New("only support pointer")
+	}
+	destValue = destValue.Elem()
+	destType := reflect.TypeOf(dest).Elem()
+
+	return unmarshal(src, destValue, destType)
+}
+
+func unmarshal(src map[string]any, dValue reflect.Value, dType reflect.Type) (err error) {
+	for i := 0; i < dValue.NumField(); i++ {
+		fieldValue := dValue.Field(i)
+		fieldType := dType.Field(i)
+
+		if fieldType.Anonymous {
+			unmarshal(src, fieldValue, fieldValue.Type())
+			continue
+		}
+
+		var key string
+		var options []string
+		if tag := fieldType.Tag.Get(keyName); tag != "" {
+			arr := strings.Split(tag, ",")
+			key = arr[0]
+			options = arr[1:]
+		} else {
+			key = fieldType.Name
+		}
+
+		if v, ok := src[key]; ok {
+			srcValue := reflect.ValueOf(v)
+			if fieldValue.Kind() == srcValue.Kind() {
+				fieldValue.Set(srcValue)
+			} else {
+				return ErrNotMatch
+			}
+		} else if !slicex.Containers("optional", options) {
+			return errors.New("field <" + key + "> is required")
+		}
 	}
 
 	return
 }
+
+// func Set(value reflect.Value, v any) (err error) {
+// 	vv := reflect.ValueOf(v)
+// 	// fmt.Printf("%+v\n", value.Type().Name())
+// 	if value.Kind() == reflect.Invalid || value.Kind() == vv.Kind() {
+// 		value.Set(vv)
+// 	}
+
+// 	return
+// }
--- util/mapx/mapx_test.go
+++ util/mapx/mapx_test.go
@@ -1,24 +1,80 @@
 package mapx
 
 import (
-	"fmt"
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestSetStruct(t *testing.T) {
-	type a struct {
-		A int
+func TestUnmarshal1(t *testing.T) {
+	type test struct {
+		A int `input:"a"`
 	}
 
-	var test *a
+	m := map[string]any{
+		"a": 1,
+	}
+	var a test
 
-	test2 := a{
-		A: 1,
+	Unmarshal(m, &a)
+	require.Equal(t, 1, a.A)
+}
+
+func TestUnmarshal2(t *testing.T) {
+	s := make([]int, 0, 2)
+	s = append(s, 1)
+	type test struct {
+		A []int `input:"a"`
 	}
 
-	v := reflect.ValueOf(test).Elem()
-	Set(v, test2)
+	m := map[string]any{
+		"a": s,
+	}
+	var a test
 
-	fmt.Printf("%+v\n", test)
+	Unmarshal(m, &a)
+	require.Equal(t, s, a.A)
+}
+
+func TestUnmarshal3(t *testing.T) {
+	type test2 struct {
+		A int `input:"a"`
+	}
+	type test struct {
+		test2
+	}
+
+	m := map[string]any{
+		"a": 1,
+	}
+	var a test
+
+	err := Unmarshal(m, &a)
+	require.Nil(t, err)
+	require.Equal(t, 1, a.A)
+}
+
+func TestUnmarshal4(t *testing.T) {
+	type test struct {
+		A int `input:"a"`
+	}
+
+	m := map[string]any{}
+	var a test
+
+	err := Unmarshal(m, &a)
+	require.NotNil(t, err)
+}
+
+func TestUnmarshal5(t *testing.T) {
+	type test struct {
+		A int `input:"a,optional"`
+	}
+
+	m := map[string]any{}
+	var a test
+
+	err := Unmarshal(m, &a)
+	require.Nil(t, err)
+	require.Equal(t, 0, a.A)
 }
