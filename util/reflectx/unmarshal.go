886ea42930074fd068e8be9757ffc92afa6271a6
zed <zedsmail@163.com>
2023-10-07 00:29:54 +0800

    --

--- util/reflectx/unmarshal.go
+++ util/reflectx/unmarshal.go
@@ -12,7 +12,15 @@ func Unmarshal(src any, dest any, tags ...string) (err error) {
 }
 
 func unmarshalMap(src map[string]any, dest reflect.Value, tags ...string) (err error) {
-	for i := 0; i<dest.NumField(); i++ {
+	for i := 0; i < dest.NumField(); i++ {
+		if dest.Type().Field(i).Anonymous {
+			err = unmarshalMap(src, dest.Field(i), tags...)
+			if err != nil {
+				return
+			}
+			continue
+		}
+
 		content := GetTag(dest.Type().Field(i), tags...)
 		if content == "" {
 			continue
@@ -20,19 +28,61 @@ func unmarshalMap(src map[string]any, dest reflect.Value, tags ...string) (err e
 
 		arr := strings.Split(content, ",")
 		val, ok := src[arr[0]]
-		valValue := reflect.ValueOf(val)
 
-		if !ok || valValue.IsZero() {
+		if !ok {
 			if !isOptional(arr[1:]) {
 				err = fmt.Errorf("field [%s] is required", arr[0])
 				return
 			}
 			continue
 		}
-		dest.Field(i).Set(valValue)
+
+		switch x := val.(type) {
+		case map[string]any:
+			err = unmarshalMap(x, dest.Field(i), tags...)
+			if err != nil {
+				return
+			}
+		default:
+			valValue := reflect.ValueOf(val)
+			if valValue.IsZero() {
+				if !isOptional(arr[1:]) {
+					err = fmt.Errorf("field [%s] is required", arr[0])
+					return
+				}
+				continue
+			}
+
+			dest.Field(i).Set(convertTypeOfPtr(dest.Field(i).Type(), valValue))
+		}
 	}
 
 	return
+}
+
+func baseKind(value reflect.Value) reflect.Kind {
+	for value.Kind() == reflect.Ptr {
+		value = value.Elem()
+	}
+
+	return value.Kind()
+}
+
+func convertTypeOfPtr(tp reflect.Type, target reflect.Value) reflect.Value {
+	// keep the original value is a pointer
+	if tp.Kind() == reflect.Ptr && target.CanAddr() {
+		tp = tp.Elem()
+		target = target.Addr()
+	}
+
+	for tp.Kind() == reflect.Ptr {
+		p := reflect.New(target.Type())
+		p.Elem().Set(target)
+		target = p
+		tp = tp.Elem()
+	}
+
+	return target
 }
 
 func isOptional(arr []string) bool {
--- util/reflectx/unmarshal_test.go
+++ util/reflectx/unmarshal_test.go
@@ -10,15 +10,51 @@ func TestMap(t *testing.T) {
 	m := map[string]any{
 		"a": 1,
 		"b": "1",
+		"c": 1,
 	}
 	type testMapStruct struct {
-		A int `json:"a"`
+		A int    `json:"a"`
 		B string `json:"b"`
+		C *int   `json:"c"`
 	}
 	var mStruct testMapStruct
 
 	err := unmarshalMap(m, reflect.ValueOf(&mStruct).Elem(), "json")
 	require.Nil(t, err)
+	require.Equal(t, "1", mStruct.B)
+	require.Equal(t, 1, mStruct.A)
+	require.Equal(t, 1, *mStruct.C)
+}
+
+func TestMapStrust(t *testing.T) {
+	m := map[string]any{
+		"a": 1,
+		"b": "1",
+		"c": 1,
+		"d": 1,
+		"e": map[string]any{
+			"a": 1,
+		},
+	}
+	type testMapStruct2 struct {
+		D int `json:"d"`
+	}
+	type testMapStruct struct {
+		A int    `json:"a"`
+		B string `json:"b"`
+		C *int   `json:"c"`
+		testMapStruct2
+		E struct {
+			A int `json:"a"`
+		} `json:"e"`
+	}
+	var mStruct testMapStruct
+
+	err := unmarshalMap(m, reflect.ValueOf(&mStruct).Elem(), "json")
 	require.Nil(t, err)
-	//fmt.Printf("%+v\n",mStruct)
+	require.Equal(t, "1", mStruct.B)
+	require.Equal(t, 1, mStruct.A)
+	require.Equal(t, 1, *mStruct.C)
+	require.Equal(t, 1, mStruct.D)
+	require.Equal(t, 1, mStruct.E.A)
 }
